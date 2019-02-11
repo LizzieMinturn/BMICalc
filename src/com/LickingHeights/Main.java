a922f14f0ab698aa6b540e3278a7f183986da20b
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-02-11 08:01:11 -0500

    declared all my variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,9 +1,21 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// BMI=kg/m*m, m=inches*(0.0254/1), inch= m*(1/0.0254), pounds=1kg*(2.204/1)
-        
+        //declare
+        Scanner keyboard;
+        int inches,feet;
+        double pounds,meters,BMI,kg;
+
+        //initializing
+        keyboard = new Scanner(System.in);
+
+
+
+
     }
 }
