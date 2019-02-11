e0923d3633ede819900a0cce9b9f6bea567e31c8
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-02-11 07:48:39 -0500

    commented all of my equations

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,9 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// BMI=kg/m*m, m=inches*(0.0254/1), inch= m*(1/0.0254), pounds=1kg*(2.204/1)
+        
+    }
+}
